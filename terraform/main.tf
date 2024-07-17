ee769a16d376cdfafa7ec30945326493473d33fd
uladzimirzel <uladzimirzel@gmail.com>
2024-07-17 13:18:13 +0300

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,46 +1,15 @@
 provider "yandex" {
-  zone = "ru-central1-a"
-  service_account_key_file = "~/authorized_key.json"
-  cloud_id = "b1g5ek2k5ua5ljdio8qp"
-  folder_id = "b1ghe4idec5s333glntf"
+  zone                     = "ru-central1-a"
+  service_account_key_file = "/root/authorized_key.json"
+  cloud_id                 = "b1g5ek2k5ua5ljdio8qp"
+  folder_id                = "b1ghe4idec5s333glntf"
 }
 
-resource "yandex_compute_disk" "boot-disk" {
-  name     = "boot-disk"
-  type     = "network-ssd"
-  zone     = "ru-central1-a"
-  size     = "15"
-  image_id = "fd8t24r7o6m7fdvlp47l"
+resource "yandex_vpc_network" "test-vpc" {
+  name = "test"
 }
 
-resource "yandex_compute_instance" "vm-1" {
-  name                      = "linux-vm"
-  allow_stopping_for_update = true
-  platform_id               = "standard-v1"
-  zone                      = "ru-central1-a"
-
-  resources {
-    cores  = "2"
-    memory = "2"
-  }
-
-  boot_disk {
-    disk_id = yandex_compute_disk.boot-disk.id
-  }
-
-  network_interface {
-    subnet_id = "${yandex_vpc_subnet.default-ru-central1-a.id}"
-    nat       = true
-  }
-}
-
-resource "yandex_vpc_network" "default" {
-  name = "default"
-}
-
-resource "yandex_vpc_subnet" "default-ru-central1-a" {
-  name           = "default-ru-central1-a"
-  zone           = "ru-central1-a"
-  v4_cidr_blocks = ["10.128.0.0/24"]
-  network_id     = "${yandex_vpc_network.default.id}"
+resource "yandex_vpc_subnet" "test-subnet-a" {
+  network_id = "${yandex_vpc_subnet.test-vpc.id}"
+  v4_cidr_block = ["10.2.0.0/16"]
 }
