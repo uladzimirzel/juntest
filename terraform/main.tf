6d90cb62539bf13a89d3e3fee18ae655a1cda36a
uladzimirzel <uladzimirzel@gmail.com>
2024-07-18 13:21:26 +0300

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "yandex" {
   zone                     = "ru-central1-a"
-  service_account_key_file = "/root/key.json"
+  service_account_key_file = "/var/jenkins_home/key.json"
   cloud_id                 = "b1g5ek2k5ua5ljdio8qp"
   folder_id                = "b1ghe4idec5s333glntf"
 }
