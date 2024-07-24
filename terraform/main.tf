6b9cf314c2c36de26dd1884be6b318e462fad57e
uladzimirzel <uladzimirzel@gmail.com>
2024-07-24 11:34:46 +0300

    Add prod server

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,3 +40,29 @@ resource "yandex_compute_instance" "build" {
     ssh-keys = "root:${file("/root/.ssh/id_ed25519.pub")}"
   }
 }
+
+resource "yandex_compute_instance" "prod" {
+  name        = "build"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80bm0rh4rkepi5ksdi"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.test-subnet-a.id}"
+    nat = true
+  }
+
+  metadata = {
+    ssh-keys = "root:${file("/root/.ssh/id_ed25519.pub")}"
+  }
+}
